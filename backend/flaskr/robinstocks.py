43b3072f9c9a629ca8b65637e184ff803e9ab2c8
Hong Yan <zgshyh@gmail.com>
2020-06-30 23:01:55 -0500

    add options request from robinstocks api

--- backend/flaskr/robinstocks.py
+++ backend/flaskr/robinstocks.py
@@ -7,3 +7,7 @@ load_dotenv()
 username = os.environ['ROBIN_USERNAME']
 password = os.environ['ROBIN_PASSWORD']
 robin_stocks.login(username,password)
+optionData = robin_stocks.options.find_options_by_expiration('fb', expirationDate='2020-07-17',optionType='call')
+for item in optionData:
+    item = dict(item)
+    print(' price -',item['strike_price'],' exp - ',item['expiration_date'],' symbol - ', item['chain_symbol'],' delta - ',item['delta'],' theta - ',item['theta'])
